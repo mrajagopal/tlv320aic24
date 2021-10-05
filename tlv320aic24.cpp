8eda5af5b243a73b002ea79bbd54f07694323e0e
Madhu RAJAGOPAL <rajagopal.madhu@gmail.com>
2021-10-05 21:23:23 +1300

    Initial skeleton commit of driver

---
+++ tlv320aic24.cpp
@@ -0,0 +1,8 @@
+//
+//  tlv320aic24.cpp
+//  
+//
+//  Created by Madhu RAJAGOPAL on 5/10/21.
+//
+
+#include "tlv320aic24.hpp"
---
+++ tlv320aic24.hpp
@@ -0,0 +1,22 @@
+//
+//  tlv320aic24.hpp
+//  
+//
+//  Created by Madhu RAJAGOPAL on 5/10/21.
+//
+
+#ifndef tlv320aic24_hpp
+#define tlv320aic24_hpp
+
+#include <stdio.h>
+
+class tlv320aic24
+{
+public:
+  tlv320aic24();
+  ~tlv320aic24();
+  
+private:
+  
+}
+#endif /* tlv320aic24_hpp */
